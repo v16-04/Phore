23bfb01ccf517cd5c4dc3496a1cf03c3cae78f47
Pieter Wuille <pieter.wuille@gmail.com>
2016-06-12 18:54:15 +0200

    Cache transaction cost in mempool

--- src/tinyformat.h
+++ src/tinyformat.h
@@ -779,7 +779,6 @@ inline const char* FormatIterator::streamStateFromFormat(std::ostream& out,
         break;
     case 'X':
         out.setf(std::ios::uppercase);
-        [[gnu::fallthrough]];
     case 'x':
     case 'p':
         out.setf(std::ios::hex, std::ios::basefield);
@@ -787,20 +786,17 @@ inline const char* FormatIterator::streamStateFromFormat(std::ostream& out,
         break;
     case 'E':
         out.setf(std::ios::uppercase);
-        [[gnu::fallthrough]];
     case 'e':
         out.setf(std::ios::scientific, std::ios::floatfield);
         out.setf(std::ios::dec, std::ios::basefield);
         break;
     case 'F':
         out.setf(std::ios::uppercase);
-        [[gnu::fallthrough]];
     case 'f':
         out.setf(std::ios::fixed, std::ios::floatfield);
         break;
     case 'G':
         out.setf(std::ios::uppercase);
-        [[gnu::fallthrough]];
     case 'g':
         out.setf(std::ios::dec, std::ios::basefield);
         // As in boost::format, let stream decide float format.
